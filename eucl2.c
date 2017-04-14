6922177719987705ed8fd8f84a3f2991ecb9c7c8
Jimmy Pickboy <catman85@users.noreply.github.com>
2017-04-15 01:06:45 +0300

    Update eucl2.c

--- eucl2.c
+++ eucl2.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-// C συνάρτηση για την ανεπτυγμένη μορφή του
-// Αλγορίθμου του Ευκλείδη
+
 int efkl2(int a, int b, int *x, int *y)
 {
     // Base Case
@@ -12,7 +11,7 @@ int efkl2(int a, int b, int *x, int *y)
         return b;
     }
 
-    int x1, y1; //για την αναδρομική
+    int x1, y1; 
     int gcd = efkl2(b%a, a, &x1, &y1);
 
     // Update x and y using results of recursive
@@ -22,7 +21,7 @@ int efkl2(int a, int b, int *x, int *y)
     return gcd;
 }
 
-//οδηγίες χρήσης
+
 void usage(void){
         printf("Type 2 integers with a space between them and hit Enter\n");
 }
@@ -30,7 +29,6 @@ void usage(void){
 int main(int argc, char *argv[])
 {
     int x, y, a, b;
-     //δεχόμαστε input και σαν arguments απο το command line
      if(argc!=3){
                 while(scanf("%d%d",&a, &b)!=2){
                         usage(); 	//οδηγίες χρήσης
@@ -40,8 +38,6 @@ int main(int argc, char *argv[])
                 a=atoi(argv[1]);
                 b=atoi(argv[2]);
      }
-    //επειδή μια συνάρτηση δέν μπορεί να επιστρέφει πολλές τιμές
-    //της δίνουμε σάν ορίσματα τις διευθύνσεις των x,y
     int g = efkl2(a, b, &x, &y);
     printf("gcd(%d, %d) = %d = %d*%d + %d*%d\n",
            a, b, g, x, a, y, b);
